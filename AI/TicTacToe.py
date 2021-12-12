1df8d1d6a670ac2893a480940422a243cd9046f6
Vineeth B V <71359072+vineethbv@users.noreply.github.com>
2021-12-12 18:18:16 +0530

    Create TicTacToe.py

---
+++ AI/TicTacToe.py
@@ -0,0 +1,128 @@
+import numpy
+
+board = numpy.zeros((3, 3))
+
+for i in range(1, 4):
+    for j in range(1, 4):
+        board[i - 1][j - 1] = str(i * 10 + j)
+
+# [[11. 12. 13.]
+#  [21. 22. 23.]
+#  [31. 32. 33.]]
+
+
+def checkWin(position, x):
+    row, col = position
+
+    win = numpy.array([x, x, x])
+
+    # check row
+    if (board[row - 1] == win).all():
+        return [1, x]
+
+    # check col
+    if ([board[0][col - 1], board[1][col - 1], board[2][col - 1]] == win).all():
+        return [1, x]
+
+    # check diag
+    if [row, col] in [[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]]:
+
+        # check \ diag
+        if ([board[0, 0], board[1, 1], board[2, 2]] == win).all():
+            return [1, x]
+
+        # check / diag
+        if ([board[0, 2], board[1, 1], board[2, 0]] == win).all():
+            return [1, x]
+
+    return [0, x]
+
+
+def inputPosition(x):
+    while True:
+        pos = int(input("Position:"))
+
+        col = int(pos % 10)
+        pos = pos / 10
+        row = int(pos % 10)
+
+        if board[row - 1][col - 1] not in [0, 1]:
+            board[row - 1][col - 1] = x
+            return [row, col]
+
+        else:
+            print("Choose an empty position!")
+
+
+def AIinputPosition(x):
+    # check if theres a slot to win
+    for row in range(1, 4):
+        for col in range(1, 4):
+            if board[row - 1][col - 1] not in [0, 1]:
+                temp = board[row - 1][col - 1]
+                board[row - 1][col - 1] = 0
+
+                if checkWin([row, col], 0) == [1, 0]:
+                    return [row, col]
+                else:
+                    board[row - 1][col - 1] = temp
+
+    # check if there's a slot for the human to win
+    for row in range(1, 4):
+        for col in range(1, 4):
+            if board[row - 1][col - 1] not in [0, 1]:
+                temp = board[row - 1][col - 1]
+                board[row - 1][col - 1] = 1
+
+                if checkWin([row, col], 1) == [1, 1]:
+                    board[row - 1][col - 1] = 0
+                    return [row, col]
+                else:
+                    board[row - 1][col - 1] = temp
+
+    # put it in a corner lol
+    for [row, col] in [[0, 0], [0, 2], [1, 0], [1, 2], [2, 0], [2, 2]]:
+        if board[row - 1][col - 1] not in [0, 1]:
+            board[row - 1][col - 1] = 0
+            return [row, col]
+
+    # check for center
+    for [row, col] in [1, 1]:
+        if board[row - 1][col - 1] not in [0, 1]:
+            board[row - 1][col - 1] = 0
+            return [row, col]
+
+    # put wherever empty rip
+    for row in range(1, 4):
+        for col in range(1, 4):
+            if board[row - 1][col - 1] not in [0, 1]:
+                board[row - 1][col - 1] = 0
+                return [row, col]
+
+
+for j in range(1, 10):
+    i = j % 2
+    print(board)
+    # AI - 0
+    # HUMAN - 1
+
+    if i == 1:
+        print("\nHUMAN")
+        position = inputPosition(i)
+    else:
+        print("\nAI")
+        position = AIinputPosition(i)
+
+    hasWon = checkWin(position, i)
+
+    if hasWon[0] == 1:
+        print(board)
+        if hasWon[1] == 1:
+            print("HUMAN WINS!! WOOHOOO!!!")
+        else:
+            print("AI WINS. WHAT A BUMMER...")
+        exit(1)
+
+
+print("Draw lmao")
+exit(1)
