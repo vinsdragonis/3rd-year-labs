821363163bc9e7ff9771515f9b090324aef61077
Vineeth B V <71359072+vineethbv@users.noreply.github.com>
2021-12-13 02:22:16 +0530

    Create temp_sensor.ino

---
+++ IoT/WEEK - 4/temp_sensor.ino
@@ -0,0 +1,19 @@
+int APin = 0;
+float celsius;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  float rawVoltage = analogRead(APin);
+  float millivolts = (rawVoltage/1024)*5000;
+  celsius = millivolts/10;
+  
+  Serial.println(celsius);
+  Serial.print("Degree Celsius \n");
+  Serial.print((celsius*9)/5 +32);
+  Serial.print("degree Farenheit\n");
+  delay(3000);
+}
