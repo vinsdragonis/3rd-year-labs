b129200ea075f160aa970ddbd04b9f56162b8438
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-12-13 18:12:58 +0530

    Update IDDFS_8puzzle.py

--- AI/IDDFS_8Puzzle/IDDFS_8puzzle.py
+++ AI/IDDFS_8Puzzle/IDDFS_8puzzle.py
@@ -1,58 +1,52 @@
 from collections import defaultdict
 
+
 class Graph:
-    def __init__(self,vertices):
-        self.noOfVertex = vertices
+    def __init__(self, vertices):
+        self.V = vertices
         self.graph = defaultdict(list)
 
-    def addEdge(self,start,end):
-        self.graph[start].append(end)
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
 
-    def search(self,src,target,maxDepth):
-        if src == target:
+    def DLS(self, src, target, limit):
+
+        if(src == target):
             return True
-        
-        if maxDepth<=0:
+
+        if(limit <= 0):
             return False
-        
+
         for i in self.graph[src]:
-            if self.search(i, target, maxDepth-1):
+            if(self.DLS(i, target, limit-1)):
                 return True
-        
+
         return False
 
-    def iterativeSearch(self,src,target,maxDepth):
-        for i in range(maxDepth):
-            if self.search(src, target, i):
+    def IDDFS(self, src, target, limit):
+        for i in range(limit):
+            if(self.DLS(src, target, i)):
                 return True
-        
         return False
 
-if __name__ == "__main__":
-    print("Enter number of vertex: ", end="")
-    n = int(input())
 
-    g = Graph(n)
+k = 7
+g = Graph(int(k))
+for i in range(int(k)-1):
+    g.addEdge(0,1)
+    g.addEdge(0,2)
+    g.addEdge(1,3)
+    g.addEdge(1,4)
+    g.addEdge(2,5)
+    g.addEdge(2,6)
 
-    print("Enter no of edges: ", end="")
-    noOfEdge = int(input())
+target = 6
+maxDepth = 3
+src = 0
 
-    print("Enter edges: ")
-
-    for i in range(noOfEdge):
-        edge = list(map(int, input().split(" ")))
-        g.addEdge(edge[0] ,edge[1])
-
-    print("Enter src vertex: ", end="")
-    src = int(input())
-
-    print("Enter target vertex: ", end="")
-    target = int(input())
-
-    print("Enter maxDepth: ", end="")
-    maxDepth = int(input())
-
-    if g.iterativeSearch(src target, maxDepth):
-        print("Can reach :D")
-    else:
-        print("Cannot reach :'(")
+if g.IDDFS(int(src), int(target), int(maxDepth)) == True:
+    print("Target is reachable from source " +
+          "within the given max depth")
+else:
+    print("Target is NOT reachable from source " +
+          "within the given max depth!")
