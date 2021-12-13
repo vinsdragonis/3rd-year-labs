aa5df5f6376761964ad1f811984f74cef952dbe8
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2021-12-13 18:56:39 +0530

    Update aStar_8Puzzle.py

--- AI/A*_8Puzzle/aStar_8Puzzle.py
+++ AI/A*_8Puzzle/aStar_8Puzzle.py
@@ -3,9 +3,9 @@ def print_grid(src):  # print the grid
     state[state.index(-1)] = ' '
     print(
         f"""
-{state[0]} {state[1]} {state[2]}
-{state[3]} {state[4]} {state[5]}
-{state[6]} {state[7]} {state[8]}
+		{state[0]} {state[1]} {state[2]}
+		{state[3]} {state[4]} {state[5]}
+		{state[6]} {state[7]} {state[8]}
         """
     )
 
